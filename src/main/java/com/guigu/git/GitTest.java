74d021389feecfde408d6d3a0c6cf8897dbf75f5
weilin <jiuzy0@163.com>
2022-04-14 14:11:24 +0800

    解决bug

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.guigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git3");
+        System.out.println("hello git hotfix");
     }
 }
