401cb68074e921d2f930fa87e7040db6c735fe90
weilin <jiuzy0@163.com>
2022-04-14 14:33:10 +0800

    hotfix修改4

--- src/main/java/com/guigu/git/GitTest.java
+++ src/main/java/com/guigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git hotfix");
         System.out.println("hello git hotfix11222");
+        System.out.println("hello git hotfix3333");
     }
 }
